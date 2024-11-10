6e77578c35aef3656800dadc0faed7eea5976509
thien2218 <thien1367526@gmail.com>
2024-11-10 20:35:26 +0700

    Remove redundant funcs

--- main.py
+++ main.py
@@ -1,31 +1,26 @@
 import cv2
 
 # Method to draw boundary around the detected feature
-def draw_boundary(img, classifier, scale_factor, min_neighbors, color, text):
+def draw_boundary(img, classifier, scale_factor, min_neighbors, text):
     # Converting image to gray-scale
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # detecting features in gray-scale image, returns coordinates, width and height of features
     features = classifier.detectMultiScale(gray_img, scale_factor, min_neighbors)
     # drawing rectangle around the feature and labeling it
     for (x, y, w, h) in features:
-        cv2.rectangle(img, (x,y), (x+w, y+h), color, 2)
+        cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,255), 2)
         cv2.putText(img, text, (x, y-4), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 1, cv2.LINE_AA)
 
-# Method to detect the features
-def detect(img, faceCascade):
-    color = {"blue":(255,0,0), "red":(0,0,255), "green":(0,255,0), "white":(255,255,255)}
-    draw_boundary(img, faceCascade, 1.1, 10, color['blue'], "Face")
     return img
-
 
 # Loading classifiers
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-cap = cv2.VideoCapture(0);
+cap = cv2.VideoCapture(0)
 
 while True:
     _, img = cap.read()
-    img = detect(img, faceCascade)
+    img = draw_boundary(img, faceCascade, 1.1, 10, "Human")
     cv2.imshow("counter", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
